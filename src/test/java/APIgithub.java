dea53613479c6ddcc3803803efa6147baf2855e9
selcukarikan <selcukarikan1301@gmail.com>
2022-09-19 08:55:35 +0200

    inital commit

---
+++ src/test/java/APIgithub.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class APIgithub {
+
+    @Test
+    public void deneme(){
+
+        //https://jsonplaceholder.typicode.com/posts/44
+        //GET request gönderdigimizde status code 200
+        //content type JSON
+        //response bodysinde bulunan userId nin 5
+        //response body sinde bulunan title nin "optio dolar molestias sit"
+        //oldugunu test ediniz
+
+        //1-Request URL ve Body olustur...
+
+    }}
